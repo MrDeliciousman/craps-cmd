7a77d8cc9b2514b833a9e7cd82ad4db9a58b0599
Thomas Herrera <rayherrera90@outlook.com>
2019-02-05 08:44:20 -0700

    Switched to Mersenne Twister.

--- src/edu/cnm/deepdive/craps/controller/Craps.java
+++ src/edu/cnm/deepdive/craps/controller/Craps.java
@@ -4,9 +4,12 @@ import edu.cnm.deepdive.craps.model.Game;
 import edu.cnm.deepdive.craps.model.State;
 import java.util.Random;
 import java.util.Scanner;
+import org.apache.commons.rng.simple.JDKRandomBridge;
+import org.apache.commons.rng.simple.RandomSource;
 
 public class Craps {
 
+  private static final int SEED_SIZE = 312;
   private Random rng;
   private Game game;
 
@@ -28,7 +31,7 @@ public class Craps {
   }
 
   public Craps() {
-    rng = new Random();
+    rng = new JDKRandomBridge(RandomSource.MT_64, RandomSource.createIntArray(SEED_SIZE));
     game = new Game(rng);
   }
   public void play(boolean verbose) {
